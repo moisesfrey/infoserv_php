ccd183a14db6dc4f43fe5fa8cdd19d2f0b1a3e2a
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-08-04 21:44:49 -0300

    exercicios aula 1 e aula2

---
+++ aula1.php
@@ -0,0 +1,12 @@
+ <?php
+
+ $nome = "Moisés";
+ $idade = 14;
+ $cidade = "Bento Gonçalves";
+ $curso = "PHP";
+
+ echo "Ola, " . $nome . ". Sua idade é: " . $idade . ". A cidade é: " . $cidade . ". O curso é: " . $curso; 
+ //Ola, Moisés. Sua idade é: 14. A cidade é: Bento Gonçalves. O curso é: PHP
+
+
+ 
