57faca06fbb4157f6576b2b0ad5ebd6c8e47be60
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-08-18 21:55:55 -0300

    tabuada e numeros pares

--- aula 5.php
+++ aula 5.php
@@ -16,8 +16,25 @@
 /**
  * calcular e exibir os 5 primeiros numeros primos 
  * dica: utilizar formula dos numeros pares 
-
  */
 
+$tabuadado5 = 5;
 
+for ($i = 1; $i <= 10; $i++ ) {
+    $resultado = $numero * $i;
+    echo "$numero X $i = $resultado <br>";
+}
 
+echo "<br>";
+
+for ($numero = 2; $numero <=50; $numereo++) {
+    $resto = $numero % 2;
+    $ehpar = 
+    $resto ==0;
+
+    if($ehpar){
+        echo "o $numero é par. <br>";
+    }
+}
+
+echo "<br>";
